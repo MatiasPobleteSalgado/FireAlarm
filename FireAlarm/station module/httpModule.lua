6efa5136bb7bebefaf9390fab5cdc455b5a1d0a9
IvanTopp <itopp2015@alu.uct.cl>
2017-10-28 22:36:33 -0300

    Actualización httpModule
    Actualización del módulo http.

--- FireAlarm/station module/httpModule.lua
+++ FireAlarm/station module/httpModule.lua
@@ -2,13 +2,13 @@ local httpModule = {}
 
 local function httpPost(url,dict, callback)
     http.post(url,
-        "Content-Type: application/x-www-form-urlencoded\r\nAccept: application/json\r\n",
+        "Content-Type: application/json\r\nAccept: application/json\r\n",
         cjson.encode(dict),
         function (code,data)
             if (code < 0) then
                 print('{"ERROR":"PostError","Message":"No se pudo realizar el post"}')
             else
-                callback(code,data)
+                callback(data)
             end
         end)
 end
